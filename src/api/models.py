ca93f83ffde758aaa8fa15373f6c0e583ef5d267
orosell666 <o.rosell.ribas@gmail.com>
2022-03-25 19:30:27 +0000

    base de datos funciona, lista

--- src/api/models.py
+++ src/api/models.py
@@ -3,76 +3,90 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__= 'user'
+    
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     name = db.Column(db.String(80), nullable=False)
-    lastName = db.Column(db.String(120), unique=True, nullable=False)
-    phonenumber = db.Column(db.String(120), unique=True, nullable=False) 
+    lastName = db.Column(db.String(120), nullable=False)
+    phonenumber = db.Column(db.String(120), unique=True, nullable=False)
+    license = db.Column(db.String(120), unique=True, nullable=False)
+    adress = db.Column(db.String(120), nullable=False)
+    birthdate = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.email
+        return  self.email
 
     def serialize(self):
         return {
-            "id": self.id,
+            "user_id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
 
 class Moto(db.Model):
-    __tablename__= 'moto'
+   
     id = db.Column(db.Integer, primary_key=True)
-    #modelo1 = db.Column(db.String(120), nullable=False)
     power = db.Column(db.Integer, nullable=False)
-    brand = db.Column(db.String(120), nullable=False)
-   # tipo1 = db.Column(db.String(120), nullable=False)
     priceday = db.Column(db.Integer, nullable=False)
     priceweek = db.Column(db.Integer, nullable=False)
-    discount1 = db.Column(db.Integer, nullable=False)
-    discount2 = db.Column(db.Integer, nullable=False)
-    comment = db.Column(db.String(300), nullable=False)
+    discount_weekend = db.Column(db.Integer, nullable=True)
+    discount_week = db.Column(db.Integer, nullable=True)
+    comment = db.Column(db.String(300), nullable=True)
     provincia = db.Column(db.String(300), nullable=False)
     ciudad = db.Column(db.String(300), nullable=False)
     direccion = db.Column(db.String(300), nullable=False)
     latitud = db.Column(db.Integer, primary_key=True)
     longitud = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(7), nullable=True)
+
     
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
         nullable=False) 
-    user = db.relationship('User', backref='user', lazy=True)
+    user = db.relationship('User', backref='user', lazy=True, cascade = "all,delete")
+
+    modelo_id = db.Column(db.Integer, db.ForeignKey('modelo.id'),
+        nullable=False) 
+    modelo = db.relationship('Modelo', backref='modelo', lazy=True, cascade = "all,delete")
+
+    tipo_id = db.Column(db.Integer, db.ForeignKey('tipo.id'),
+        nullable=False) 
+    tipo = db.relationship('Tipo', backref='tipo', lazy=True, cascade = "all,delete")
+
+
 
     def __repr__(self):
         return '<Moto %r>' % self.id
 
 
 class Modelo(db.Model):
-    __tablename__= 'modelo'
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
-    
+    marca_id = db.Column(db.Integer, db.ForeignKey('marca.id'),
+       nullable=False)
     
     def __repr__(self):
-        return '<Modelo %r>' % self.id
+        return  self.name
 
 class Tipo(db.Model):
-    __tablename__= 'tipo'
+   
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), primary_key=True)
+    name = db.Column(db.String(120), nullable=False )
 
 
     def __repr__(self):
-        return '<Tipo %r>' % self.id
+        return self.name
 
 class Marca(db.Model):
-    __tablename__= 'marca'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), primary_key=True)
+   
+    id = db.Column(db.Integer, primary_key=True, nullable=False )
+    name = db.Column(db.String(120), nullable=False)
+    modelo = db.relationship('Modelo', backref='marca', lazy=True, cascade = "all,delete")
 
     
 
     def __repr__(self):
-        return '<Marca %r>' % self.id
+        return self.name
